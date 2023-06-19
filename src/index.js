bd1c725f77f51e9075df6c72f949aa6ec622e09b
mmttyrm <mmttyrm@gmail.com>
2023-06-20 00:35:43 +0700

    update-3

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//http logger
 app.use(morgan('combined'));
 
 //template engine
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    })
+);
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 //route
 app.get('/', (req, res) => {
     res.render('home');
+});
+app.get('/new', (req, res) => {
+    res.render('new');
 });
 
 //127.0.0.1 - localhost
